ce47b3c3b6679d61cb5be37ea18dd8dd6ca56839
doet <doet@doets-iMac.local>
2016-02-25 19:59:45 +0700

    First commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass')
-;
+	sass = require('gulp-sass');
+	
 
-gulp.task('sass', function() {
-    // content
-    gulp.src('dev/scss/style.scss')
-      .pipe(sass({errLogToConsole: true}))
-      .pipe(gulp.dest('./dist/css'))
-});
+	gulp.task('sass', function() {
+	    // content
+	    gulp.src('dev/scss/style.scss')
+	      .pipe(sass({errLogToConsole: true}))
+	      
+	      .pipe(gulp.dest('./dist/css'));
+	})
 
-gulp.task('watch',['sass'], function() {
-    // content
-    gulp.watch('dev/scss/style.css', ['sass'])
-});
+	gulp.task('watch', function() {
+	    // content
+	    gulp.watch('dev/scss/style.scss', ['sass'])
+	})
 
-gulp.task('default',['sass', 'watch']);
+	gulp.task('default',['sass', 'watch']);
